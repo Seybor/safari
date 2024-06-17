55a7ebd3c1ebd91a7cdc93a2b74f1731e3a2aa4c
Seybor <sashaborisenko1996@gmail.com>
2024-06-17 18:15:30 +0700

    initial

---
+++ app/js/libs.js
@@ -0,0 +1,4 @@
+// ! =include
+
+// include simple-slider/itc-slider.min.js
+// include wow/wow.min.js
---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+import { s, all } from './modules/base.js'
+import * as UI from './modules/ui.js'
+import * as DOM from './modules/util-dom.js'
+import * as UTIL from './modules/util-others.js'
+
+// $('body').hide()
+
+// UI.changeTheme()
+// UI.burger()
+// UI.accordionStart()
+// UI.tabStart()
+
+// DOM.eraseInput()
+// DOM.removeAddClass()
+// DOM.wrapper()
+
+// UTIL.generateRandomNumbers()
+// UTIL.getRandomNumber()
+// UTIL.shuffleArr()
+
