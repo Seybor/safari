713adca2d3b92cf92f800102aebd962b82a614d7
Seybor <sashaborisenko1996@gmail.com>
2024-06-28 14:54:55 +0700

    14.54

--- app/js/pages/account.js
+++ app/js/pages/account.js
@@ -1,24 +1,29 @@
 import { s, all } from '../modules/base.js'
 
 const getAccountPage = () => {
-	all('.account-dashboard__item').forEach(e => {
-		e.addEventListener('click', (evt) => {
-			evt.preventDefault()
 
-			if (e.classList.contains('account-dashboard__item--active')) {
-				return
-			} else {
-				s('.account-dashboard__item--active').classList.remove('account-dashboard__item--active')
-				e.classList.add('account-dashboard__item--active')
-			}
+	if (s('.account')) {
+		all('.account-dashboard__item').forEach(e => {
+			e.addEventListener('click', (evt) => {
+				evt.preventDefault()
 
+				if (e.classList.contains('account-dashboard__item--active')) {
+					return
+				} else {
+					s('.account-dashboard__item--active').classList.remove('account-dashboard__item--active')
+					e.classList.add('account-dashboard__item--active')
+				}
+
+			})
 		})
-	})
 
-	s('.info-form__wrap-link').addEventListener('click', (evt) => {
-		evt.preventDefault()
-		s('.info-form__right').classList.toggle('info-form__right--active')
-	})
+		s('.info-form__wrap-link').addEventListener('click', (evt) => {
+			evt.preventDefault()
+			s('.info-form__right').classList.toggle('info-form__right--active')
+		})
+	}
+
+
 
 
 }
