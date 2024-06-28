8d1285fab6408362dd7d6d7fa8afd4eb9d2a7789
Seybor <sashaborisenko1996@gmail.com>
2024-06-28 19:43:54 +0700

    2h

--- app/js/pages/account.js
+++ app/js/pages/account.js
@@ -3,6 +3,7 @@ import { s, all } from '../modules/base.js'
 const getAccountPage = () => {
 
 	if (s('.account')) {
+
 		all('.account-dashboard__item').forEach(e => {
 			e.addEventListener('click', (evt) => {
 				evt.preventDefault()
@@ -12,6 +13,9 @@ const getAccountPage = () => {
 				} else {
 					s('.account-dashboard__item--active').classList.remove('account-dashboard__item--active')
 					e.classList.add('account-dashboard__item--active')
+
+					s('.account-content__type--active').classList.remove('account-content__type--active')
+					s(`.account-content__type[data-id="${e.dataset.id}"]`).classList.add('account-content__type--active')
 				}
 
 			})
@@ -21,6 +25,8 @@ const getAccountPage = () => {
 			evt.preventDefault()
 			s('.info-form__right').classList.toggle('info-form__right--active')
 		})
+
+
 	}
 
 
